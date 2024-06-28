13c8ac793d3d71b69e484615ab32be979029acdc
Kanishk Pandey <135948593+Sinaker@users.noreply.github.com>
2024-06-28 21:14:14 +0530

    Testing out POST and GET Requests

---
+++ backend/controllers/feed.js
@@ -0,0 +1,42 @@
+const { validationResult } = require("express-validator");
+
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "This is the first post!",
+        imageUrl: "images/Coverpage.jpg",
+        creator: { name: "Kanishk" },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    // If any error is found then return 422 error
+
+    return res.status(422).json({
+      message: "Validation failed, entered data is incorrect",
+      errors: errors.array(),
+    });
+  }
+  // Create post in db
+  res.status(201).json({
+    message: "Post created successfully!",
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: { name: "Dummy" },
+      createdAt: new Date(),
+    },
+  });
+};
